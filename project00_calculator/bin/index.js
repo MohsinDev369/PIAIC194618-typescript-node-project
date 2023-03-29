7e067787736c67e91b740a5eca25cf5e207c2aeb
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-03-29 23:27:22 +0500

    alpha stage

--- project00_calculator/bin/index.js
+++ project00_calculator/bin/index.js
@@ -1,3 +1,23 @@
 #! /usr/bin/env node
-console.log('Mohsin is here');
-export {};
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+console.log(chalk.green('Welcome to MohsinDev calculator'));
+const questions = [
+    {
+        type: 'input',
+        name: 'equation',
+        message: "Enter your math problem",
+    },
+];
+inquirer.prompt(questions).then((answers) => {
+    let { equation } = answers;
+    let equationInArray = equation.split('');
+    let solution = 1;
+    if (equationInArray[1] === '+') {
+        solution = parseInt(equationInArray[0]) + parseInt(equationInArray[2]);
+    }
+    else if (equationInArray[1] === '-') {
+        solution = parseInt(equationInArray[0]) - parseInt(equationInArray[2]);
+    }
+    console.log(solution);
+});
--- project00_calculator/src/index.ts
+++ project00_calculator/src/index.ts
@@ -1,2 +1,32 @@
 #! /usr/bin/env node
-console.log('Mohsin is here');
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+
+console.log(chalk.green('Welcome to MohsinDev calculator'));
+
+const questions = [
+    {
+    type: 'input',
+    name: 'equation',
+    message: "Enter your math problem",
+  },
+]
+
+inquirer.prompt(questions).then((answers) => {
+  let solution = 1
+  // extract equation 
+  let { equation } = answers
+  // ['number','operation','number'] in equationInArray
+  let equationInArray = equation.split('')
+  // check for the opration  
+  if (equationInArray[1] === '+') {
+    solution = parseInt(equationInArray[0]) + parseInt(equationInArray[2])
+  } else if (equationInArray[1] === '-') {
+    solution = parseInt(equationInArray[0]) - parseInt(equationInArray[2])
+  } else if (equationInArray[1] === '/') {
+    solution = parseInt(equationInArray[0]) / parseInt(equationInArray[2])
+  } else if (equationInArray[1] === '%') {
+    solution = parseInt(equationInArray[0]) % parseInt(equationInArray[2])
+  }
+  console.log(`Answer: ${solution}`);
+});
