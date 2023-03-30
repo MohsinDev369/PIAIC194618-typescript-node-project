c36d54d33e6cf455c2b31e70b6df20f4e8a2688c
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-03-30 22:58:04 +0500

    DON 30-3-2023

--- project01_number_guessing-game/bin/index.js
+++ project01_number_guessing-game/bin/index.js
@@ -1,82 +1,36 @@
 #! /usr/bin/env node
 import inquirer from 'inquirer';
-let score = 0;
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
+import chalk from 'chalk';
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 9);
 }
-function playGame(rounds) {
-    console.log(`Starting game with ${rounds} rounds...`);
-    for (let i = 1; i <= rounds; i++) {
-        const numToGuess = getRandomInt(1, 11);
-        console.log(`Round ${i}: Guess a number between 1 and 10:`);
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'guess',
-                message: 'Your guess:'
-            }
-        ]).then((answers) => {
-            const guess = parseInt(answers.guess);
-            if (guess === numToGuess) {
-                console.log(`Congratulations! You guessed the number correctly.`);
-                score++;
-            }
-            else {
-                console.log(`Sorry, the number was ${numToGuess}. Better luck next time!`);
-            }
-            if (i === rounds) {
-                console.log(`Game over. Your score is ${score}.`);
-                inquirer.prompt([
-                    {
-                        type: 'confirm',
-                        name: 'playAgain',
-                        message: 'Would you like to play again?'
-                    }
-                ]).then((answers) => {
-                    if (answers.playAgain) {
-                        inquirer.prompt([
-                            {
-                                type: 'input',
-                                name: 'rounds',
-                                message: 'How many rounds would you like to play?',
-                                validate: function (value) {
-                                    const parsedValue = parseInt(value);
-                                    if (Number.isInteger(parsedValue) && parsedValue > 0) {
-                                        return true;
-                                    }
-                                    return 'Please enter a positive integer';
-                                }
-                            }
-                        ]).then((answers) => {
-                            playGame(parseInt(answers.rounds));
-                        });
-                    }
-                    else {
-                        console.log('Thanks for playing!');
-                    }
-                });
-            }
-            else {
-                playGame(rounds);
-            }
-        });
-    }
-}
-inquirer.prompt([
-    {
-        type: 'input',
+async function playGame() {
+    const answer = await inquirer.prompt({
+        type: 'number',
         name: 'rounds',
-        message: 'Welcome to the number guessing game! How many rounds would you like to play?',
-        validate: function (value) {
-            const parsedValue = parseInt(value);
-            if (Number.isInteger(parsedValue) && parsedValue > 0) {
-                return true;
-            }
-            return 'Please enter a positive integer';
+        message: 'How many rounds do you want to play?'
+    });
+    const rounds = answer.rounds;
+    let score = 0;
+    for (let i = 1; i <= rounds; i++) {
+        const randomNumber = generateRandomNumber();
+        const guessAnswer = await inquirer.prompt({
+            type: 'number',
+            name: 'guess',
+            message: `Round ${i}: Guess a number between 1 and 9`
+        });
+        const guess = guessAnswer.guess;
+        if (isNaN(guess) || guess < 1 || guess > 9) {
+            console.log(chalk.red('Invalid guess. Please enter a number between 1 and 9.'));
+        }
+        else if (guess === randomNumber) {
+            score++;
+            console.log(chalk.green('Correct!'));
+        }
+        else {
+            console.log(chalk.red(`Wrong! The number was ${randomNumber}.`));
         }
     }
-]).then((answers) => {
-    playGame(parseInt(answers.rounds));
-});
+    console.log(chalk.yellow(`Your score is ${score}/${rounds}.`));
+}
+playGame();
--- project01_number_guessing-game/src/index.ts
+++ project01_number_guessing-game/src/index.ts
@@ -2,6 +2,45 @@
 import inquirer from 'inquirer';
 import chalk from 'chalk';
 
+function generateRandomNumber(): number {
+    return Math.floor(Math.random() * 9);
+}
+
+async function playGame() {
+    const answer = await inquirer.prompt({
+        type: 'number',
+        name: 'rounds',
+        message: 'How many rounds do you want to play?'
+    });
+    const rounds = answer.rounds;
+
+    let score = 0;
+    for (let i = 1; i <= rounds; i++) {
+        const randomNumber = generateRandomNumber();
+        const guessAnswer = await inquirer.prompt({
+            type: 'number',
+            name: 'guess',
+            message: `Round ${i}: Guess a number between 1 and 9`
+        });
+        const guess = guessAnswer.guess;
+        if (isNaN(guess) || guess < 1 || guess > 9) {
+            console.log(chalk.red('Invalid guess. Please enter a number between 1 and 9.'));
+        } else if (guess === randomNumber) {
+            score++;
+            console.log(chalk.green('Correct!'));
+        } else {
+            console.log(chalk.red(`Wrong! The number was ${randomNumber}.`));
+        }
+    }
+
+    console.log(chalk.yellow(`Your score is ${score}/${rounds}.`));
+}
+
+playGame();
+
+
+
+
 
 
 
