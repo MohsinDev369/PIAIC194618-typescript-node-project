42606f662939e497c1930a0ff6c1ef52f36dbb68
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-03-30 22:08:17 +0500

    start 2

--- project01_number_guessing-game/bin/index.js
+++ project01_number_guessing-game/bin/index.js
@@ -1,42 +1,82 @@
 #! /usr/bin/env node
 import inquirer from 'inquirer';
-import chalk from 'chalk';
-console.log(chalk.red('Welcome to ' + chalk.green('MohsinDev') + ' calculator'));
-console.log('Enter the problem as:' + chalk.yellow(' number') + chalk.cyan(' operation sign') + chalk.yellow(' number'));
-const questions = [
+let score = 0;
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+function playGame(rounds) {
+    console.log(`Starting game with ${rounds} rounds...`);
+    for (let i = 1; i <= rounds; i++) {
+        const numToGuess = getRandomInt(1, 11);
+        console.log(`Round ${i}: Guess a number between 1 and 10:`);
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'guess',
+                message: 'Your guess:'
+            }
+        ]).then((answers) => {
+            const guess = parseInt(answers.guess);
+            if (guess === numToGuess) {
+                console.log(`Congratulations! You guessed the number correctly.`);
+                score++;
+            }
+            else {
+                console.log(`Sorry, the number was ${numToGuess}. Better luck next time!`);
+            }
+            if (i === rounds) {
+                console.log(`Game over. Your score is ${score}.`);
+                inquirer.prompt([
+                    {
+                        type: 'confirm',
+                        name: 'playAgain',
+                        message: 'Would you like to play again?'
+                    }
+                ]).then((answers) => {
+                    if (answers.playAgain) {
+                        inquirer.prompt([
+                            {
+                                type: 'input',
+                                name: 'rounds',
+                                message: 'How many rounds would you like to play?',
+                                validate: function (value) {
+                                    const parsedValue = parseInt(value);
+                                    if (Number.isInteger(parsedValue) && parsedValue > 0) {
+                                        return true;
+                                    }
+                                    return 'Please enter a positive integer';
+                                }
+                            }
+                        ]).then((answers) => {
+                            playGame(parseInt(answers.rounds));
+                        });
+                    }
+                    else {
+                        console.log('Thanks for playing!');
+                    }
+                });
+            }
+            else {
+                playGame(rounds);
+            }
+        });
+    }
+}
+inquirer.prompt([
     {
         type: 'input',
-        name: 'equation',
-        message: "Enter your math problem:",
-        validate: async (input) => {
-            if (input === '') {
-                return 'Enter a equation like 2+2';
+        name: 'rounds',
+        message: 'Welcome to the number guessing game! How many rounds would you like to play?',
+        validate: function (value) {
+            const parsedValue = parseInt(value);
+            if (Number.isInteger(parsedValue) && parsedValue > 0) {
+                return true;
             }
-            if (input.length > 3 || input.length < 3) {
-                return 'two numbers only';
-            }
-            return true;
+            return 'Please enter a positive integer';
         }
-    },
-];
-inquirer.prompt(questions).then((answers) => {
-    let solution = 1;
-    // extract equation 
-    let { equation } = answers;
-    // ['number','operation','number'] in equationInArray
-    let equationInArray = equation.split('');
-    // check for the opration  
-    if (equationInArray[1] === '+') {
-        solution = parseInt(equationInArray[0]) + parseInt(equationInArray[2]);
     }
-    else if (equationInArray[1] === '-') {
-        solution = parseInt(equationInArray[0]) - parseInt(equationInArray[2]);
-    }
-    else if (equationInArray[1] === '/') {
-        solution = parseInt(equationInArray[0]) / parseInt(equationInArray[2]);
-    }
-    else if (equationInArray[1] === '%') {
-        solution = parseInt(equationInArray[0]) % parseInt(equationInArray[2]);
-    }
-    console.log(`Answer is: ` + chalk.red(solution));
+]).then((answers) => {
+    playGame(parseInt(answers.rounds));
 });
--- project01_number_guessing-game/src/index.ts
+++ project01_number_guessing-game/src/index.ts
@@ -2,41 +2,9 @@
 import inquirer from 'inquirer';
 import chalk from 'chalk';
 
-console.log(chalk.red('Welcome to '+ chalk.green('MohsinDev')+' calculator'));
-console.log('Enter the problem as:' + chalk.yellow(' number') + chalk.cyan(' operation sign') + chalk.yellow(' number'));
-const questions = [
-    {
-    type: 'input',
-    name: 'equation',
-    message: "Enter your math problem:",
-    validate: async (input: string) => {
-      if (input === '') {
-        return 'Enter a equation like 2+2';
-      }
-      if (input.length > 3 || input.length < 3) {
-        return 'two numbers only';
-      }
-      return true;
-    }
-  },
-]
 
-inquirer.prompt(questions).then((answers) => {
-  let solution = 1
-  // extract equation 
-  let { equation } = answers
-  // ['number','operation','number'] in equationInArray
-  let equationInArray = equation.split('')
-  // check for the opration  
-  if (equationInArray[1] === '+') {
-    solution = parseInt(equationInArray[0]) + parseInt(equationInArray[2])
-  } else if (equationInArray[1] === '-') {
-    solution = parseInt(equationInArray[0]) - parseInt(equationInArray[2])
-  } else if (equationInArray[1] === '/') {
-    solution = parseInt(equationInArray[0]) / parseInt(equationInArray[2])
-  } else if (equationInArray[1] === '%') {
-    solution = parseInt(equationInArray[0]) % parseInt(equationInArray[2])
-  }
-  console.log(`Answer is: ` + chalk.red(solution));
-  
-});
+
+
+
+
+
