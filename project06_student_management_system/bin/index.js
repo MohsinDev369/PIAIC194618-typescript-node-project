3727b2cd664fd1ce328c2b0e165f43dd3c9a4fc7
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-05-02 23:16:41 +0500

    add student don

--- project06_student_management_system/bin/index.js
+++ project06_student_management_system/bin/index.js
@@ -1,23 +1,84 @@
 #! /usr/bin/env node
 import inquirer from 'inquirer';
 import chalk from 'chalk';
-const countWordsAndChars = (paragraph) => {
-    const words = paragraph.split(' ').filter(word => word !== '').length;
-    const characters = paragraph.split(' ').join('').length;
-    return { words, characters };
-};
-const promptParagraph = async () => {
-    console.log(chalk.blue('Welcome to mohsinDev word Counter'));
-    const questions = [
+class Student {
+    name;
+    id;
+    courses;
+    balance;
+    constructor(name, id, courses, balance) {
+        this.name = name;
+        this.id = id;
+        this.courses = courses;
+        this.balance = balance;
+    }
+    enroll(course) {
+        this.courses.push(course);
+    }
+    viewBalance() {
+        console.log(`Balance: ${this.balance}`);
+    }
+    payTuition(amount) {
+        this.balance -= amount;
+        console.log(`Thank you for your payment of ${amount}. Your new balance is ${this.balance}.`);
+    }
+    showStatus() {
+        console.log(`Name: ${this.name}`);
+        console.log(`ID: ${this.id}`);
+        console.log(`Courses: ${this.courses.join(', ')}`);
+        console.log(`Balance: ${this.balance}`);
+    }
+}
+const students = [];
+const main = async () => {
+    const menuPrompt = [
         {
-            type: 'input',
-            name: 'paragraph',
-            message: 'Enter a paragraph:',
+            type: 'list',
+            name: 'menu',
+            message: 'Which currency to convert to?',
+            choices: [
+                { name: `Add Student `, value: 'addStudent' },
+                { name: `Enroll Student `, value: 'enrollStudent' },
+                { name: `View Balance `, value: 'viewBalance' },
+                { name: `Pay Tuition `, value: 'payTuition' },
+                { name: `Show Status `, value: 'showStatus' },
+                { name: "Exit", value: 'Exit' }
+            ],
         },
     ];
-    const { paragraph } = await inquirer.prompt(questions);
-    const { words, characters } = countWordsAndChars(paragraph);
-    console.log((chalk.green(`Words (excluding whitespaces): ${words}`)));
-    console.log(chalk.green(`Characters (excluding whitespaces): ${characters}`));
+    const { menu } = await inquirer.prompt(menuPrompt);
+    switch (menu) {
+        case 'addStudent': {
+            addStudent();
+        }
+    }
+    console.log(menu);
 };
-promptParagraph();
+console.log(chalk.blue('Welcome to mohsinDev student Management System'));
+main();
+async function addStudent() {
+    const { fullName } = await inquirer.prompt({
+        type: 'input',
+        name: 'fullName',
+        message: 'Enter student name:',
+    });
+    const { course } = await inquirer.prompt({
+        type: 'list',
+        name: 'course',
+        message: 'Select course:',
+        choices: [
+            { name: `English `, value: 'addStudent' },
+            { name: `Math `, value: 'enrollStudent' },
+            { name: `History `, value: 'viewBalance' },
+        ],
+    });
+    const { balance } = await inquirer.prompt({
+        type: 'input',
+        name: 'balance',
+        message: 'Enter Your Balance:',
+    });
+    const id = Math.floor(Math.random() * 90000) + 1000;
+    students.push(new Student(fullName, id, course, balance));
+    console.log(students);
+    main();
+}
--- project06_student_management_system/src/index.ts
+++ project06_student_management_system/src/index.ts
@@ -2,29 +2,95 @@
 import inquirer from 'inquirer';
 import chalk from 'chalk';
 
-const countWordsAndChars = (paragraph: string): { words: number, characters: number } => {
-    const words = paragraph.split(' ').filter(word => word !== '').length;
-    const characters = paragraph.split(' ').join('').length;
+class Student {
+    name: string;
+    id: number;
+    courses: string[];
+    balance: number;
 
-    return { words, characters };
-};
+    constructor(name: string, id: number, courses: string[], balance: number) {
+        this.name = name;
+        this.id = id;
+        this.courses = courses;
+        this.balance = balance;
+    }
 
-const promptParagraph = async () => {
-    console.log(chalk.blue('Welcome to mohsinDev word Counter'));
-    const questions = [
+    enroll(course: string) {
+        this.courses.push(course);
+    }
+
+    viewBalance() {
+        console.log(`Balance: ${this.balance}`);
+    }
+
+    payTuition(amount: number) {
+        this.balance -= amount;
+        console.log(`Thank you for your payment of ${amount}. Your new balance is ${this.balance}.`);
+    }
+
+    showStatus() {
+        console.log(`Name: ${this.name}`);
+        console.log(`ID: ${this.id}`);
+        console.log(`Courses: ${this.courses.join(', ')}`);
+        console.log(`Balance: ${this.balance}`);
+    }
+}
+const students: Student[] = [];
+
+const main = async () => {
+    const menuPrompt = [
         {
-            type: 'input',
-            name: 'paragraph',
-            message: 'Enter a paragraph:',
+            type: 'list',
+            name: 'menu',
+            message: 'Which currency to convert to?',
+            choices: [
+                { name: `Add Student `, value: 'addStudent' },
+                { name: `Enroll Student `, value: 'enrollStudent' },
+                { name: `View Balance `, value: 'viewBalance' },
+                { name: `Pay Tuition `, value: 'payTuition' },
+                { name: `Show Status `, value: 'showStatus' },
+                { name: "Exit", value: 'Exit' }
+            ],
         },
     ];
+    const { menu }:{menu:string} = await inquirer.prompt(menuPrompt);
 
-    const { paragraph } = await inquirer.prompt(questions);
-
-    const { words, characters } = countWordsAndChars(paragraph);
-
-    console.log((chalk.green(`Words (excluding whitespaces): ${words}`)));
-    console.log(chalk.green(`Characters (excluding whitespaces): ${characters}`));
+    switch (menu) {
+        case 'addStudent': {
+            addStudent()
+        }
+    }
+    console.log(menu);
 };
 
-promptParagraph();
+console.log(chalk.blue('Welcome to mohsinDev student Management System'));
+main();
+
+async function addStudent() {
+    const { fullName } = await inquirer.prompt({
+        type: 'input',
+        name: 'fullName',
+        message: 'Enter student name:',
+    });
+    const { course } = await inquirer.prompt({
+        type: 'list',
+        name: 'course',
+        message: 'Select course:',
+        choices: [
+            { name: `English `, value: 'English' },
+            { name: `Math `, value: 'Math' },
+            { name: `History `, value: 'History' },
+        ],
+    });
+    const { balance } = await inquirer.prompt({
+        type: 'input',
+        name: 'balance',
+        message: 'Enter Your Balance:',
+    });
+    const id = Math.floor(Math.random() * 90000) + 1000;
+    students.push(new Student(fullName, id, course, balance))
+    console.log(students);
+    main()
+}
+
+
